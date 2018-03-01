8962973cdcfc815e1adc72009b9bcc1a158361d7
Marek Wawreniuk <marek.wawreniuk@websensa.com>
2018-03-01 21:02:18 +0100

    Add searching function

--- main.cpp
+++ main.cpp
@@ -18,7 +18,8 @@ typedef struct tup {
 set<array<int, 9>> tups_s;
 vector<array<int, 9>> tups_v;
 set<array<int, 2>> starts;
-set<array<int, 5>> vehicles;
+set<array<int, 4>> vehicles;
+array<vector<int>, 10000> vehicles_starts;
 array<int, 10000> done;
 int done_count = 0;
 
@@ -30,8 +31,12 @@ void add_tuple(int a, int b, int x, int y, int s, int f) {
     tupi[3] = y;
     tupi[4] = s;
     tupi[5] = f;
-    tups_s.insert(tupi);
+//    tups_s.insert(tupi);
     tups_v.push_back(tupi);
+}
+
+void add_start(int a, int b) {
+    starts.insert({a, b});
 }
 
 void parse() {
@@ -43,9 +48,10 @@ void parse() {
     for (int i = 0; i < N; ++i) {
         cin >> a >> b >> x >> y >> s >> f;
         add_tuple(a, b, x, y, s, f);
+        add_start(a, b);
     }
     if (DEBUG) {
-        for (set<array<int, 9>>::iterator it = tups_s.begin(); it != tups_s.end(); it++) {
+        for (set<array<int, 9>>::iterator it = tups_v.begin(); it != tups_v.end(); it++) {
             for (int i = 0; i < 6; ++i) {
                 cout << (*it)[i] << " ";
             }
@@ -56,12 +62,45 @@ void parse() {
 
 void create_vehicles() {
     for (int i = 0; i < F; ++i) {
-        vehicles.insert({0, 0, 0, 0, 0});
+        vehicles.insert({0, 0, 0, i});
     }
+}
+
+void assign_starts() {
+    vector<array<int, 9>>::iterator veh = vehicles.begin();
+    int v = (*veh)[5];
+    if (done[v]) {
+        vehicles.erase(vehicles.begin());
+        return;
+    }
+    int start = -1;
+    int max = 0;
+    int temp, when = 0, best_when = 0;
+    int x, best_x;
+    int y, best_y;
+    for (vector<array<int, 9>>::iterator it = tups_v.begin(); it != tups_v.end(); it++) {
+        if (temp = isok(veh, it, &when, &x, &y)) {
+            if (temp > max) {
+                max = temp;
+                start = (*it)[8];
+                best_when = when;
+                best_x = x;
+                best_y = y;
+            }
+        }
+    }
+    if (start != -1) {
+        vehicles_starts[v].push_back(start);
+    }
+    vehicles.erase(vehicles.begin());
+    vehicles.insert({best_when, best_x, best_y, v});
 }
 
 int main() {
     parse();
     create_vehicles();
+    while (!starts.empty() && !vehicles.empty()) {
+        assign_starts();
+    }
     return 0;
 }
